7b9af1e365567bdf3168e3f221d0c1728bf57523
stutisaurya <stuti.saurya@gmail.com>
2021-06-22 15:28:25 +0530

    Added:AddEmpWage

--- src/com/EmpWage/EmployeeWageCalculation.java
+++ src/com/EmpWage/EmployeeWageCalculation.java
@@ -4,19 +4,21 @@ public class EmployeeWageCalculation {
 	public static void main(String args[])
 
 	{
-		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 
 		int empHrs = 0;
 		int empWage = 0;
 
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+
 		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
-		else
-			empHrs = 0;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage: " + empWage);
+		System.out.println("Employee Wage : " + empWage);
 	}
 
 }
