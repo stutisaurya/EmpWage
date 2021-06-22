acb1c277e722cd9a943072110f78b0d083c2f57b
stutisaurya <stuti.saurya@gmail.com>
2021-06-22 15:45:53 +0530

    Added:Refactor

--- src/com/EmpWage/EmployeeWageCalculation.java
+++ src/com/EmpWage/EmployeeWageCalculation.java
@@ -1,32 +1,36 @@
 package com.EmpWage;
 
 public class EmployeeWageCalculation {
-
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-
 	public static void main(String[] args) {
 
-		int empHrs = 0;
-		int empWage = 0;
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
 
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		int emphrs = 0;
+		int empwage = 0;
 
-		switch (empCheck) {
-		case IS_FULL_TIME:
-			empHrs = 8;
+		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+
+		switch (empcheck) {
+		case 1:
+			emphrs = 8;
+			System.out.println("Employee is present full time");
 			break;
-		case IS_PART_TIME:
-			empHrs = 4;
+
+		case 2:
+			emphrs = 4;
+			System.out.println("Employee is present part time");
 			break;
+
 		default:
-			empHrs = 0;
+			emphrs = 0;
+			System.out.println("Employee is absent");
+			break;
+
 		}
 
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage : " + empWage);
-
+		empwage = emphrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage : " + empwage);
 	}
-
 }
