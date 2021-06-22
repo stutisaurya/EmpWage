fb151e0b922d0891e8cbbabc35ca44fe23091492
stutisaurya <stuti.saurya@gmail.com>
2021-06-22 15:50:55 +0530

    Added:EmpwageMonth

--- src/com/EmpWage/EmployeeWageCalculation.java
+++ src/com/EmpWage/EmployeeWageCalculation.java
@@ -6,31 +6,38 @@ public class EmployeeWageCalculation {
 		int IS_FULL_TIME = 1;
 		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
-
 		int emphrs = 0;
 		int empwage = 0;
+		int i;
+		int total_wage = 0;
+		for (i = 1; i < 21; i++) {
+			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empcheck) {
+			case 1:
+				emphrs = 8;
+				System.out.println("Employee is present full time");
+				break;
 
-		switch (empcheck) {
-		case 1:
-			emphrs = 8;
-			System.out.println("Employee is present full time");
-			break;
+			case 2:
+				emphrs = 4;
+				System.out.println("Employee is present part time");
+				break;
 
-		case 2:
-			emphrs = 4;
-			System.out.println("Employee is present part time");
-			break;
+			default:
+				emphrs = 0;
+				System.out.println("Employee is absent");
+				break;
 
-		default:
-			emphrs = 0;
-			System.out.println("Employee is absent");
-			break;
+			}
+
+			empwage = emphrs * EMP_RATE_PER_HOUR;
+			System.out.println("Employee Wage : " + empwage);
+			total_wage = total_wage + empwage;
 
 		}
 
-		empwage = emphrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage : " + empwage);
+		System.out.println("Total Monthly Wage : " + total_wage);
+
 	}
 }
