8374d9753c56211b8b62c6e9c80d2ada62480438
stutisaurya <stuti.saurya@gmail.com>
2021-06-22 16:22:11 +0530

    Added: Calculate Wage

--- src/com/EmpWage/EmployeeWageCalculation.java
+++ src/com/EmpWage/EmployeeWageCalculation.java
@@ -2,9 +2,7 @@ package com.EmpWage;
 
 public class EmployeeWageCalculation {
 	public static void main(String[] args) {
-
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
+		// variables
 		int EMP_RATE_PER_HOUR = 20;
 		int emphrs = 0;
 		int empwage = 0;
