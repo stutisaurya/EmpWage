9582516402bd2007bd5f1a3491f6c9ab873b9c90
stutisaurya <stuti.saurya@gmail.com>
2021-06-22 15:38:59 +0530

    Added:SwitchCase

--- src/com/EmpWage/EmployeeWageCalculation.java
+++ src/com/EmpWage/EmployeeWageCalculation.java
@@ -1,24 +1,32 @@
 package com.EmpWage;
 
 public class EmployeeWageCalculation {
-	public static void main(String args[])
 
-	{
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+
+	public static void main(String[] args) {
 
 		int empHrs = 0;
 		int empWage = 0;
 
-		double empCheck = Math.floor(Math.random() * 10) % 3;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-		if (empCheck == IS_FULL_TIME)
+		switch (empCheck) {
+		case IS_FULL_TIME:
 			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
+			break;
+		case IS_PART_TIME:
 			empHrs = 4;
+			break;
+		default:
+			empHrs = 0;
+		}
+
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee Wage : " + empWage);
+
 	}
 
 }
