93381e318447d6ab80f20f1cafb42d4235a5b1fd
stutisaurya <stuti.saurya@gmail.com>
2021-06-25 00:21:02 +0530

    Added:TotalWage

--- src/com/EmpWage/EmployeeWageCalculation.java
+++ src/com/EmpWage/EmployeeWageCalculation.java
@@ -1,15 +1,28 @@
 package com.EmpWage;
 
 public class EmployeeWageCalculation {
-	// Constants
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
 
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-		// Variables
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+
+	private final String company;
+	private final int wagePerHour;
+	private final int workingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public EmployeeWageCalculation(String company, int wagePerHour, int workingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.wagePerHour = wagePerHour;
+		this.workingDays = workingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void computeWage() {
+
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < workingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -23,15 +36,23 @@ public class EmployeeWageCalculation {
 				empHrs = 0;
 			}
 			totalEmpHrs += empHrs;
-			System.out.println("Day#: " + totalWorkingDays + " Emp Hrs: " + empHrs);
+			System.out.println("Day# : " + totalWorkingDays + " Emp Hr : " + empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Emp Wage: for Company: " + company + " is: " + totalEmpWage);
-		return totalEmpWage;
+		totalEmpWage = totalEmpHrs * wagePerHour;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Emp Wage for company " + company + " is " + totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmpWage("Dmart", 20, 20, 100);
-		computeEmpWage("Reliance", 10, 40, 200);
+
+		EmployeeWageCalculation dMart = new EmployeeWageCalculation("DMart", 20, 20, 100);
+		EmployeeWageCalculation reliance = new EmployeeWageCalculation("Reliance", 10, 20, 200);
+		dMart.computeWage();
+		System.out.println(dMart);
+		reliance.computeWage();
+		System.out.println(reliance);
 	}
 }
